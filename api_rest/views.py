c7db8c85226873aced7c14460356eba6963b1dd9
Lgusta11 <rainlima2020@gmail.com>
2024-08-10 18:25:51 -0300

    second commit

--- api_rest/views.py
+++ api_rest/views.py
@@ -10,4 +10,39 @@ from .serializers import UsuarioSerializador
 
 import json
 
+@api_view(['GET'])
+def get_usuarios(request):
+    
+    if request.method == 'GET':
+        
+        usuarios = Usuario.objects.all()
+        
+        serializador = UsuarioSerializador(usuarios, many=True)
+        
+        return Response(serializador.data)
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def databaseEmDjango():
+#    data = Usuario.objects.get(pk='usuario_apelido')  #OBJETO
+    
+#    data = Usuario.objects.filter(usuario_idade='17') #QUERYSET
+    
+#    data = Usuario.objects.exclude(usuario_idade='17')  #QUERYSET
+    
+#    data.save()
+    
+#    data.delete()
