5d38e6995bdf83107b1b4f89ef3efc1c9369e692
Lgusta11 <rainlima2020@gmail.com>
2024-08-17 16:17:25 -0300

    seccond commit

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from . import views
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="APIDjango_Usuario",
+      default_version='v1',
+      description="CRUD de usuario",
+      terms_of_service="#",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('', views.get_users, name='get_all_users'),
     path('user/<str:nick>', views.get_by_nick),
     path('data/', views.user_manager),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
--- api_root/settings.py
+++ api_root/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'api_rest',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
